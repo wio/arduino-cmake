e557f1e84fd117d530ddc5010dc92e625d00af9c
Timor Gruber <timor.gruber@gmail.com>
2017-10-09 00:24:02 +0300

    Renamed parameter of 'detect_arduino_version' function to match a standard. 'VAR_NAME' has been renamed to 'OUTPUT_VAR_NAME'.

--- cmake/Platform/Initialization/VersionDetector.cmake
+++ cmake/Platform/Initialization/VersionDetector.cmake
@@ -2,21 +2,20 @@
 # detect_arduino_version
 # [PRIVATE/INTERNAL]
 #
-# detect_arduino_version(VAR_NAME)
+# detect_arduino_version(OUTPUT_VAR_NAME)
 #
-#       VAR_NAME - Variable name where the detected version will be saved
+#       OUTPUT_VAR_NAME - Variable name where the detected version will be saved
 #
 # Detects the Arduino SDK Version based on the revisions.txt file. The
 # following variables will be generated:
 #
-#    ${VAR_NAME}         -> the full version (major.minor.patch)
-#    ${VAR_NAME}_MAJOR   -> the major version
-#    ${VAR_NAME}_MINOR   -> the minor version
-#    ${VAR_NAME}_PATCH   -> the patch version
+#    ${OUTPUT_VAR_NAME}         -> the full version (major.minor.patch)
+#    ${OUTPUT_VAR_NAME}_MAJOR   -> the major version
+#    ${OUTPUT_VAR_NAME}_MINOR   -> the minor version
+#    ${OUTPUT_VAR_NAME}_PATCH   -> the patch version
 #
 #=============================================================================#
-function(detect_arduino_version VAR_NAME)
-
+function(detect_arduino_version OUTPUT_VAR_NAME)
     if (ARDUINO_VERSION_PATH)
         file(READ ${ARDUINO_VERSION_PATH} RAW_VERSION)
         if ("${RAW_VERSION}" MATCHES " *[0]+([0-9]+)")
@@ -33,11 +32,10 @@ function(detect_arduino_version VAR_NAME)
             list(GET SPLIT_VERSION 1 SPLIT_VERSION_MINOR)
             list(GET SPLIT_VERSION 2 SPLIT_VERSION_PATCH)
 
-            set(${VAR_NAME} "${PARSED_VERSION}" PARENT_SCOPE)
-            set(${VAR_NAME}_MAJOR "${SPLIT_VERSION_MAJOR}" PARENT_SCOPE)
-            set(${VAR_NAME}_MINOR "${SPLIT_VERSION_MINOR}" PARENT_SCOPE)
-            set(${VAR_NAME}_PATCH "${SPLIT_VERSION_PATCH}" PARENT_SCOPE)
+            set(${OUTPUT_VAR_NAME} "${PARSED_VERSION}" PARENT_SCOPE)
+            set(${OUTPUT_VAR_NAME}_MAJOR "${SPLIT_VERSION_MAJOR}" PARENT_SCOPE)
+            set(${OUTPUT_VAR_NAME}_MINOR "${SPLIT_VERSION_MINOR}" PARENT_SCOPE)
+            set(${OUTPUT_VAR_NAME}_PATCH "${SPLIT_VERSION_PATCH}" PARENT_SCOPE)
         endif ()
     endif ()
-
 endfunction()
