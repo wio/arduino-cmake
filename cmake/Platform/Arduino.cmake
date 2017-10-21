5077e49aca607892a2a200535c4dd29b569a7c2f
Timor Gruber <timor.gruber@gmail.com>
2017-10-21 14:59:56 +0300

    Extracted 'Print' and 'Debug' functions to matching scripts under the 'Extras' directory. All 'Print' functions are located in the 'Printer' script, while the 'Debug' functions are located in the 'DebugOptions' script.

--- cmake/Platform/Arduino.cmake
+++ cmake/Platform/Arduino.cmake
@@ -294,76 +294,6 @@ include(CMakeParseArguments)
 #=============================================================================#
 
 #=============================================================================#
-#                           Print Functions
-#=============================================================================#
-
-#=============================================================================#
-# print_board_list
-# [PUBLIC/USER]
-#
-# print_board_list()
-#
-# see documentation at top
-#=============================================================================#
-function(PRINT_BOARD_LIST)
-    foreach (PLATFORM ${ARDUINO_PLATFORMS})
-        if (${PLATFORM}_BOARDS)
-            message(STATUS "${PLATFORM} Boards:")
-            print_list(${PLATFORM}_BOARDS)
-            message(STATUS "")
-        endif ()
-    endforeach ()
-endfunction()
-
-#=============================================================================#
-# print_programmer_list
-# [PUBLIC/USER]
-#
-# print_programmer_list()
-#
-# see documentation at top
-#=============================================================================#
-function(PRINT_PROGRAMMER_LIST)
-    foreach (PLATFORM ${ARDUINO_PLATFORMS})
-        if (${PLATFORM}_PROGRAMMERS)
-            message(STATUS "${PLATFORM} Programmers:")
-            print_list(${PLATFORM}_PROGRAMMERS)
-        endif ()
-        message(STATUS "")
-    endforeach ()
-endfunction()
-
-#=============================================================================#
-# print_programmer_settings
-# [PUBLIC/USER]
-#
-# print_programmer_settings(PROGRAMMER)
-#
-# see documentation at top
-#=============================================================================#
-function(PRINT_PROGRAMMER_SETTINGS PROGRAMMER)
-    if (${PROGRAMMER}.SETTINGS)
-        message(STATUS "Programmer ${PROGRAMMER} Settings:")
-        print_settings(${PROGRAMMER})
-    endif ()
-endfunction()
-
-#=============================================================================#
-# print_board_settings
-# [PUBLIC/USER]
-#
-# print_board_settings(ARDUINO_BOARD)
-#
-# see documentation at top
-function(PRINT_BOARD_SETTINGS ARDUINO_BOARD)
-    if (${ARDUINO_BOARD}.SETTINGS)
-        message(STATUS "Arduino ${ARDUINO_BOARD} Board:")
-        print_settings(${ARDUINO_BOARD})
-    endif ()
-endfunction()
-
-
-#=============================================================================#
 #                         Generation Functions
 #=============================================================================#
 
@@ -1813,123 +1743,6 @@ endfunction()
 
 
 #=============================================================================#
-#                          Print Functions
-#=============================================================================#
-
-#=============================================================================#
-# print_settings
-# [PRIVATE/INTERNAL]
-#
-# print_settings(ENTRY_NAME)
-#
-#      ENTRY_NAME - name of entry
-#
-# Print the entry settings (see load_arduino_syle_settings()).
-#
-#=============================================================================#
-function(PRINT_SETTINGS ENTRY_NAME)
-    if (${ENTRY_NAME}.SETTINGS)
-
-        foreach (ENTRY_SETTING ${${ENTRY_NAME}.SETTINGS})
-            if (${ENTRY_NAME}.${ENTRY_SETTING})
-                message(STATUS "   ${ENTRY_NAME}.${ENTRY_SETTING}=${${ENTRY_NAME}.${ENTRY_SETTING}}")
-            endif ()
-            if (${ENTRY_NAME}.${ENTRY_SETTING}.SUBSETTINGS)
-                foreach (ENTRY_SUBSETTING ${${ENTRY_NAME}.${ENTRY_SETTING}.SUBSETTINGS})
-                    if (${ENTRY_NAME}.${ENTRY_SETTING}.${ENTRY_SUBSETTING})
-                        message(STATUS "   ${ENTRY_NAME}.${ENTRY_SETTING}.${ENTRY_SUBSETTING}=${${ENTRY_NAME}.${ENTRY_SETTING}.${ENTRY_SUBSETTING}}")
-                    endif ()
-                endforeach ()
-            endif ()
-            message(STATUS "")
-        endforeach ()
-    endif ()
-endfunction()
-
-#=============================================================================#
-# print_list
-# [PRIVATE/INTERNAL]
-#
-# print_list(SETTINGS_LIST)
-#
-#      SETTINGS_LIST - Variables name of settings list
-#
-# Print list settings and names (see load_arduino_syle_settings()).
-#=============================================================================#
-function(PRINT_LIST SETTINGS_LIST)
-    if (${SETTINGS_LIST})
-        set(MAX_LENGTH 0)
-        foreach (ENTRY_NAME ${${SETTINGS_LIST}})
-            string(LENGTH "${ENTRY_NAME}" CURRENT_LENGTH)
-            if (CURRENT_LENGTH GREATER MAX_LENGTH)
-                set(MAX_LENGTH ${CURRENT_LENGTH})
-            endif ()
-        endforeach ()
-        foreach (ENTRY_NAME ${${SETTINGS_LIST}})
-            string(LENGTH "${ENTRY_NAME}" CURRENT_LENGTH)
-            math(EXPR PADDING_LENGTH "${MAX_LENGTH}-${CURRENT_LENGTH}")
-            set(PADDING "")
-            foreach (X RANGE ${PADDING_LENGTH})
-                set(PADDING "${PADDING} ")
-            endforeach ()
-            message(STATUS "   ${PADDING}${ENTRY_NAME}: ${${ENTRY_NAME}.name}")
-        endforeach ()
-    endif ()
-endfunction()
-
-
-#=============================================================================#
-#                          Other Functions
-#=============================================================================#
-
-
-#=============================================================================#
-#                          Debug Functions
-#=============================================================================#
-
-#=============================================================================#
-# arduino_debug_on()
-# [PRIVATE/INTERNAL]
-#
-#  arduino_debug_on()
-#
-# Enables Arduino module debugging.
-#=============================================================================#
-function(ARDUINO_DEBUG_ON)
-    set(ARDUINO_DEBUG True PARENT_SCOPE)
-endfunction()
-
-#=============================================================================#
-# arduino_debug_on()
-# [PRIVATE/INTERNAL]
-#
-#  arduino_debug_off()
-#
-# Disables Arduino module debugging.
-#=============================================================================#
-function(ARDUINO_DEBUG_OFF)
-    set(ARDUINO_DEBUG False PARENT_SCOPE)
-endfunction()
-
-#=============================================================================#
-# arduino_debug_msg
-# [PRIVATE/INTERNAL]
-#
-# arduino_debug_msg(MSG)
-#
-#        MSG - Message to print
-#
-# Print Arduino debugging information. In order to enable printing
-# use arduino_debug_on() and to disable use arduino_debug_off().
-#=============================================================================#
-function(ARDUINO_DEBUG_MSG MSG)
-    if (ARDUINO_DEBUG)
-        message("## ${MSG}")
-    endif ()
-endfunction()
-
-
-#=============================================================================#
 #                        Initialization Script
 #=============================================================================#
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
@@ -1937,7 +1750,9 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR}/Initializat
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR}/Extras)
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR}/Core)
 
-include(Initializer)
 include(VariableValidator)
+include(Initializer)
 include(ArduinoSketchToCppConverter)
 include(FlagsSetter)
+include(DebugOptions)
+include(Printer)
