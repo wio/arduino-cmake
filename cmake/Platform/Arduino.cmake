96e2484f35e5791f5d685b36fe248a9b7b6c94ee
Timor Gruber <timor.gruber@gmail.com>
2017-10-28 15:01:56 +0300

    Extracted all macros to a script file. The script, named 'Macros', is located under the 'Extras' directory and currently contains all existing macros. Also extracted the 'load_generator_settings' function to a script file named 'GeneratorSettingLoader', also located under the 'Extras' directory.

--- cmake/Platform/Arduino.cmake
+++ cmake/Platform/Arduino.cmake
@@ -376,117 +376,6 @@ endfunction()
 
 
 #=============================================================================#
-#                           Other Functions
-#=============================================================================#
-
-
-#=============================================================================#
-#                         Internal Functions
-#=============================================================================#
-
-#=============================================================================#
-#                              Macros
-#=============================================================================#
-
-#=============================================================================#
-# parse_generator_arguments
-# [PRIVATE/INTERNAL]
-#
-# parse_generator_arguments(TARGET_NAME PREFIX OPTIONS ARGS MULTI_ARGS [ARG1 ARG2 .. ARGN])
-#
-#         PREFIX     - Parsed options prefix
-#         OPTIONS    - List of options
-#         ARGS       - List of one value keyword arguments
-#         MULTI_ARGS - List of multi value keyword arguments
-#         [ARG1 ARG2 .. ARGN] - command arguments [optional]
-#
-# Parses generator options from either variables or command arguments
-#
-#=============================================================================#
-macro(PARSE_GENERATOR_ARGUMENTS TARGET_NAME PREFIX OPTIONS ARGS MULTI_ARGS)
-    cmake_parse_arguments(${PREFIX} "${OPTIONS}" "${ARGS}" "${MULTI_ARGS}" ${ARGN})
-    error_for_unparsed(${PREFIX})
-    load_generator_settings(${TARGET_NAME} ${PREFIX} ${OPTIONS} ${ARGS} ${MULTI_ARGS})
-endmacro()
-
-#=============================================================================#
-# get_mcu
-# [PRIVATE/INTERNAL]
-#
-# get_mcu(FULL_MCU_NAME, OUTPUT_VAR)
-#
-#         FULL_MCU_NAME - Board's full mcu name, including a trailing 'p' if present
-#         OUTPUT_VAR - String value in which a regex match will be stored
-#
-# Matches the board's mcu without leading or trailing characters that would rather mess
-# further processing that requires the board's mcu.
-#
-#=============================================================================#
-macro(GET_MCU FULL_MCU_NAME OUTPUT_VAR)
-    string(REGEX MATCH "^.+[^p]" ${OUTPUT_VAR} "FULL_MCU_NAME" PARENT_SCOPE)
-endmacro()
-
-#=============================================================================#
-# increment_example_category_index
-# [PRIVATE/INTERNAL]
-#
-# increment_example_category_index(OUTPUT_VAR)
-#
-#         OUTPUT_VAR - A number representing an example's category prefix
-#
-# Increments the given number by one, taking into consideration the number notation
-# which is defined (Some SDK's or OSs use a leading '0' in single-digit numbers.
-#
-#=============================================================================#
-macro(INCREMENT_EXAMPLE_CATEGORY_INDEX OUTPUT_VAR)
-    math(EXPR INC_INDEX "${${OUTPUT_VAR}}+1")
-    if (EXAMPLE_CATEGORY_INDEX_LENGTH GREATER 1 AND INC_INDEX LESS 10)
-        set(${OUTPUT_VAR} "0${INC_INDEX}")
-    else ()
-        set(${OUTPUT_VAR} ${INC_INDEX})
-    endif ()
-endmacro()
-
-
-#=============================================================================#
-#                           Load Functions
-#=============================================================================#
-
-#=============================================================================#
-# load_generator_settings
-# [PRIVATE/INTERNAL]
-#
-# load_generator_settings(TARGET_NAME PREFIX [SUFFIX_1 SUFFIX_2 .. SUFFIX_N])
-#
-#         TARGET_NAME - The base name of the user settings
-#         PREFIX      - The prefix name used for generator settings
-#         SUFFIX_XX   - List of suffixes to load
-#
-#  Loads a list of user settings into the generators scope. User settings have
-#  the following syntax:
-#
-#      ${BASE_NAME}${SUFFIX}
-#
-#  The BASE_NAME is the target name and the suffix is a specific generator settings.
-#
-#  For every user setting found a generator setting is created of the follwoing fromat:
-#
-#      ${PREFIX}${SUFFIX}
-#
-#  The purpose of loading the settings into the generator is to not modify user settings
-#  and to have a generic naming of the settings within the generator.
-#
-#=============================================================================#
-function(LOAD_GENERATOR_SETTINGS TARGET_NAME PREFIX)
-    foreach (GEN_SUFFIX ${ARGN})
-        if (${TARGET_NAME}_${GEN_SUFFIX} AND NOT ${PREFIX}_${GEN_SUFFIX})
-            set(${PREFIX}_${GEN_SUFFIX} ${${TARGET_NAME}_${GEN_SUFFIX}} PARENT_SCOPE)
-        endif ()
-    endforeach ()
-endfunction()
-
-
-#=============================================================================#
 #                        Initialization Script
 #=============================================================================#
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
@@ -502,11 +391,13 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR}/Core/Exampl
 include(VariableValidator)
 include(Initializer)
 
+include(Macros)
 include(FlagsSetter)
 include(SourceFinder)
 include(LibraryFinder)
 include(DebugOptions)
 include(Printer)
+include(GeneratorSettingsLoader)
 
 include(ArduinoSketchToCppConverter)
 include(ArduinoSketchFactory)
