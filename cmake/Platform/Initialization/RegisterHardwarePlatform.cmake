17d60adf04c92a2e24ad4da37ebc9ac63feaf506
Timor Gruber <timor.gruber@gmail.com>
2017-11-24 15:28:56 +0200

    Added ability to register custom hardware platforms. It's more a bug-fix combined with a feature, since the function to register a platform simply been inlined, defaulting 'Arduino' and 'avr' as platform and architecture. This commit allows developers to specify their own platform and architecture by setting the `PLATFORM_PATH` and `PLATFORM_ARCHITECTURE` variables BEFORE including the Arduino-CMake toolchain.

--- cmake/Platform/Initialization/RegisterHardwarePlatform.cmake
+++ cmake/Platform/Initialization/RegisterHardwarePlatform.cmake
@@ -1,96 +1,14 @@
-#=============================================================================#
-# ToDo: Document
-#=============================================================================#
-set(PLATFORM_PATH ${ARDUINO_SDK_PATH}/hardware/arduino/)
+if (NOT PLATFORM_PATH)
+    # Arduino is assumed as the default platform
+    set(PLATFORM_PATH ${ARDUINO_SDK_PATH}/hardware/arduino/)
+endif ()
 string(REGEX REPLACE "/$" "" PLATFORM_PATH ${PLATFORM_PATH})
-GET_FILENAME_COMPONENT(PLATFORM ${PLATFORM_PATH} NAME)
+GET_FILENAME_COMPONENT(VENDOR_ID ${PLATFORM_PATH} NAME)
+GET_FILENAME_COMPONENT(BASE_PATH ${PLATFORM_PATH} PATH)
 
-# platform path changed in versions 1.5 and greater
-if (ARDUINO_SDK_VERSION VERSION_GREATER 1.0.5)
-    set(PLATFORM_PATH "${PLATFORM_PATH}/avr")
+if (NOT PLATFORM_ARCHITECTURE)
+    # avr is the default architecture
+    set(PLATFORM_ARCHITECTURE "avr")
 endif ()
 
-if (NOT PLATFORM)
-    return()
-endif ()
-
-# Avoid defining a platform multiple times if it has already been defined before
-string(TOUPPER ${PLATFORM} PLATFORM)
-list(FIND ARDUINO_PLATFORMS ${PLATFORM} PLATFORM_EXISTS)
-
-if (PLATFORM_EXISTS GREATER -1)
-    return()
-endif ()
-
-set(${PLATFORM}_PLATFORM_PATH ${PLATFORM_PATH} CACHE INTERNAL "The path to ${PLATFORM}")
-set(ARDUINO_PLATFORMS ${ARDUINO_PLATFORMS} ${PLATFORM} CACHE INTERNAL "A list of registered platforms")
-
-find_file(${PLATFORM}_CORES_PATH
-        NAMES cores
-        PATHS ${PLATFORM_PATH}
-        DOC "Path to directory containing the Arduino core sources.")
-
-find_file(${PLATFORM}_VARIANTS_PATH
-        NAMES variants
-        PATHS ${PLATFORM_PATH}
-        DOC "Path to directory containing the Arduino variant sources.")
-
-find_file(${PLATFORM}_BOOTLOADERS_PATH
-        NAMES bootloaders
-        PATHS ${PLATFORM_PATH}
-        DOC "Path to directory containing the Arduino bootloader images and sources.")
-
-find_file(${PLATFORM}_PROGRAMMERS_PATH
-        NAMES programmers.txt
-        PATHS ${PLATFORM_PATH}
-        DOC "Path to Arduino programmers definition file.")
-
-find_file(${PLATFORM}_BOARDS_PATH
-        NAMES boards.txt
-        PATHS ${PLATFORM_PATH}
-        DOC "Path to Arduino boards definition file.")
-
-# some libraries are in platform path in versions 1.5 and greater
-if (ARDUINO_SDK_VERSION VERSION_GREATER 1.0.5)
-    find_file(${PLATFORM}_PLATFORM_LIBRARIES_PATH
-            NAMES libraries
-            PATHS ${PLATFORM_PATH}
-            DOC "Path to platform directory containing the Arduino libraries.")
-    set(ARDUINO_PLATFORM_LIBRARIES_PATH "${${PLATFORM}_PLATFORM_LIBRARIES_PATH}")
-else ()
-    set(ARDUINO_PLATFORM_LIBRARIES_PATH "")
-endif ()
-
-if (${PLATFORM}_BOARDS_PATH)
-    set(SETTINGS_LIST ${PLATFORM}_BOARDS)
-    set(SETTINGS_PATH "${${PLATFORM}_BOARDS_PATH}")
-    include(LoadArduinoPlatformSettings)
-endif ()
-
-if (${PLATFORM}_PROGRAMMERS_PATH)
-    set(SETTINGS_LIST ${PLATFORM}_PROGRAMMERS)
-    set(SETTINGS_PATH "${${PLATFORM}_PROGRAMMERS_PATH}")
-    include(LoadArduinoPlatformSettings)
-endif ()
-
-if (${PLATFORM}_VARIANTS_PATH)
-    file(GLOB sub-dir ${${PLATFORM}_VARIANTS_PATH}/*)
-    foreach (dir ${sub-dir})
-        if (IS_DIRECTORY ${dir})
-            get_filename_component(variant ${dir} NAME)
-            set(VARIANTS ${VARIANTS} ${variant} CACHE INTERNAL "A list of registered variant boards")
-            set(${variant}.path ${dir} CACHE INTERNAL "The path to the variant ${variant}")
-        endif ()
-    endforeach ()
-endif ()
-
-if (${PLATFORM}_CORES_PATH)
-    file(GLOB sub-dir ${${PLATFORM}_CORES_PATH}/*)
-    foreach (dir ${sub-dir})
-        if (IS_DIRECTORY ${dir})
-            get_filename_component(core ${dir} NAME)
-            set(CORES ${CORES} ${core} CACHE INTERNAL "A list of registered cores")
-            set(${core}.path ${dir} CACHE INTERNAL "The path to the core ${core}")
-        endif ()
-    endforeach ()
-endif ()
+include(RegisterSpecificHardwarePlatform)
